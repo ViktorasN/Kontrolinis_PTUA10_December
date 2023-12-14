07d1cb77bdd2f4137065c416638349601b6eca10
Viktoras Nikitin <viktornktn@gmail.com>
2023-12-14 19:21:01 +0200

    papildziau_saliu_projekta

--- main.py
+++ main.py
@@ -5,5 +5,13 @@ class Country:
         self.area = area
         self.is_big = self.check_if_big()
 
+
+
     def check_if_big(self):
         return self.population > 2000000 or self.area > 3000000
+
+australia = Country("Australia", 23545500, 7692024)
+print("australia.is_Big")
+
+andorra = Country("Andorra", 76098, 468)
+print('andorra.is_Big')
